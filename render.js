82c0e1dd9dae67d81aad7b1515bd8de9babfe509
Robert Martin <rdm@google.com>
2013-01-24 15:32:55 -0500

    add render.{html,js} is some intermediate state

---
+++ render.js
@@ -0,0 +1,45 @@
+
+(function(exports, windowWidth, windowHeight) {
+  var generateData = function() {
+    var data = [];
+    var orig = [4, 65, 23, 80, 71, 22, 43, 10, 54, 85, 56, 17];
+    var t = Date.now();
+    for (var i in orig) {
+      data.push({ time: t, value: orig[i] });
+      t += 1;
+    }
+    return data;
+  };
+
+  var render = exports.render = function(config) {
+    d3.csv(config.csvFile, function(data) {
+      renderData(data);
+    });
+  };
+
+  var renderData = function(data) {
+    console.log(data);
+    return;
+
+    var width = (windowWidth - 50) / data.length;
+        height = windowHeight - 50;
+
+    var xScale = d3.scale.linear().domain([0, 1]).range([0, width]),
+        yScale = d3.scale.linear().domain([0, 100]).rangeRound([0, height]);
+
+    var graph = d3.select('body').append('svg')
+        .attr('class', 'graph')
+        .attr('width', width * data.length - 1)
+        .attr('height', height)
+
+    graph.selectAll('rect')
+        .data(data)
+        .enter().append('rect')
+            .attr('x', function(d, i) { return xScale(i) - 0.5; })
+            .attr('y', function(d) { return height - yScale(d.value) - 0.5; })
+            .attr('width', width)
+            .attr('height', function(d) { return yScale(d.value); });
+
+  };
+
+})(window, window.innerWidth, window.innerHeight);
